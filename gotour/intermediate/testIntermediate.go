99d9d2ec3fe2fedace2104e4f15614cfde03054b
sherin stephen <sherin@sherin-mac.local>
2020-02-25 20:39:32 -0800

    more slices

--- gotour/intermediate/testIntermediate.go
+++ gotour/intermediate/testIntermediate.go
@@ -6,6 +6,7 @@ import "fmt"
 RunIntermediate : entry for intermediate
 */
 func RunIntermediate() {
+
 	fmt.Println("***** INTERMEDIATE ******")
 
 	fmt.Println("--- testPointers ---")
@@ -50,20 +51,45 @@ func testStruct() {
 	fmt.Println(c2)
 }
 
+/**
+Notes:
+1. array is passed by value
+2. to pass by refernce , pass a pointer to array
+3. arr := [2]int -> pointer to array is *arr [2]int
+4. as shown above, pointer type is exactly [2]int , so have to be explicitly specified.
+*/
 func testArray() {
+
 	arr := [2]int{1, 2}
 	fmt.Println(arr)
 
 	var arr2 [2]string
 	arr2[0], arr2[1] = "elt1", "elt2"
+	fmt.Println(arr2) // pass by value
 
-	fmt.Println(arr2)
+	var arr3 = [...]int{1, 2, 3, 4, 5}
+	fmt.Println(arr3)
+
+	// pass by reference
+	arr4 := [1]string{"hello"}
+	_processArray(&arr2)
+	//_processArray(&arr4) // error
 
 }
+func _processArray(ptr *[2]string) {
+	fmt.Println(ptr)
+}
 
+/**
+Notes:
+- built on arrays
+- slices are by default pass by reference
+
+https://blog.golang.org/go-slices-usage-and-internals
+*/
 func testSlices() {
-	// slices
-	primes := [6]int{2, 3, 5, 7, 11, 13}
+	// A slice literal is declared just like an array literal, except you leave out the element count
+	primes := []int{2, 3, 5, 7, 11, 13}
 	var s []int = primes[1:4] // 3,5,7
 	fmt.Println(s)
 	s[0] = 10 // 3 -> 10  // slices are references backed by original array
@@ -91,11 +117,17 @@ func testSlices() {
 		fmt.Println("nil!")
 	}
 
-	// slice with make
+	// slice with make([]T, len, cap)
 	dSlice := make([]int, 5) // cap = 5, len = 5
 	fmt.Println(dSlice, cap(dSlice), len(dSlice))
+
 	dSlice2 := make([]string, 0, 3) // cap = 3, len = 0
 	fmt.Println(dSlice2, cap(dSlice2), len(dSlice2))
+
+	// slice from array
+	x := [3]string{"a", "b", "c"}
+	sl := x[:] // a slice referencing the storage of x
+	fmt.Println(sl)
 
 	// append slice
 	var sApnd []int
--- gotour/start.go
+++ gotour/start.go
@@ -1,11 +1,25 @@
 package main
 
-import (
-	"sherin.dev/gotour/basic"
-	"sherin.dev/gotour/intermediate"
-)
+import "fmt"
+
+// import (
+// 	"sherin.dev/gotour/basic"
+// 	"sherin.dev/gotour/intermediate"
+// )
 
 func main() {
-	basic.RunBasic()
-	intermediate.RunIntermediate()
+	//basic.RunBasic()
+	//intermediate.RunIntermediate()
+
+	scrap()
+}
+
+func scrap() {
+	arr := [5]int{1, 2, 3, 4, 5}
+	process(&arr)
+	fmt.Println(arr)
+
+}
+func process(a *[5]int) {
+	a[4] = 20
 }
